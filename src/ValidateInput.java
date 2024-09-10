0bea5a06ca1bfb8e89943337a3f3fb458d228503
EmmaAdabra <adabraemma@gmail.com>
2024-09-11 00:43:00 +0100

    modified ValidateInput interface
    Added a javadoc to describe the function of the
    interface

--- src/ValidateInput.java
+++ src/ValidateInput.java
@@ -1,3 +1,8 @@
+/**
+ * Interface that defines methods for validating user input.
+ * Classes implementing this interface are expected to provide
+ * specific implementations for validating various types of user input.
+ */
 public interface ValidateInput {
     String validateName(String name);
     String validateEmail(String email);
