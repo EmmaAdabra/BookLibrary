4b12cc294be30ae899fc74a5ffc0d383cca0ce98
EmmaAdabra <adabraemma@gmail.com>
2024-09-11 00:36:23 +0100

    Add ValidateInput interface
    Define ValidateInput interface as a contract for validating user input,
    outlining the methods that implementing classes must define.

---
+++ src/ValidateInput.java
@@ -0,0 +1,6 @@
+public interface ValidateInput {
+    String validateName(String name);
+    String validateEmail(String email);
+    String validatePassword(String password);
+    byte validateOption(byte userName);
+}
