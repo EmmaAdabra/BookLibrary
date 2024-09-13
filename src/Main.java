cce6e1764f1dd42a29d843ae4bb91f954aedd7c4
EmmaAdabra <adabraemma@gmail.com>
2024-09-13 12:18:18 +0100

    Create an instance of RegisterUser class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String []args){
         System.out.println("------------------ Virtual Library ----------------");
+        var validate = new ValidateUserInput();
+        var registerUser = new RegisterUser(validate);
+        registerUser.registrationUI();
     }
 }
