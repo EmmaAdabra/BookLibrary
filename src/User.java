d9061a77635f8e8414f8cb4ec662b0c265fa2861
EmmaAdabra <adabraemma@gmail.com>
2024-09-13 21:33:21 +0100

    Defined Getters for userName, userEmail and userPassword fields.

--- src/User.java
+++ src/User.java
@@ -9,22 +9,34 @@ import java.util.ArrayList;
  * It provides methods for logging in and out.
  */
 public class User {
-    private String UserName;
-    private String UserEmail;
-    private String UserPassword;
+    private String userName;
+    private String userEmail;
+    private String userPassword;
     private boolean isOnline;
     private ArrayList<Object> borrowedBook = new ArrayList<>();
 
     public User(String name, String email, String password) {
-        this.UserName = name;
-        this.UserEmail = email;
-        this.UserPassword = password;
+        this.userName = name;
+        this.userEmail = email;
+        this.userPassword = password;
     }
 
     public void login(String email, String password) {
         isOnline = true;
-    };
+    }
     public void logout() {
         isOnline = false;
-    };
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
 }
