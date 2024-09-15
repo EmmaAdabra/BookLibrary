a525fb8690bffafe409f9daeb0dcdf672b93a150
EmmaAdabra <adabraemma@gmail.com>
2024-09-15 10:55:44 +0100

    Instantiate the library class to test functionalities

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
+import java.util.HashMap;
+
 public class Main {
     public static void main(String []args){
-        System.out.println("------------------ Virtual Library ----------------");
+        System.out.println("**** Virtual Library ****");
         var validate = new ValidateUserInput();
-        var registerUser = new RegisterUser(validate);
-        registerUser.registrationUI();
+//        var registerUser = new RegisterUser(validate);
+//        registerUser.registrationUI();
+        var library = new Library(validate);
+        library.libraryUI();
     }
 }
