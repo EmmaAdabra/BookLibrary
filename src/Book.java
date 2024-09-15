5e1dfe2e8629e2e80bfaf382b55cfcddadf3eaae
EmmaAdabra <adabraemma@gmail.com>
2024-09-15 10:51:01 +0100

    Modify Book class
    - Modified the constructor signature to include ISBN and book quantity.
    - Modified the return string for toString method to include book quantity
    - Defined setters and getters for some fields

--- src/Book.java
+++ src/Book.java
@@ -1,17 +1,23 @@
 import java.time.LocalDate;
 
 public class Book {
-    private final String title;
-    private final String author;
+    private String title;
+    private String author;
     private byte bookID;
-    private final String category;
+    private String category;
+    private String ISBN;
+    private byte amountBorrowed = 0;
+    private byte quantity;
     private LocalDate dateAdded;
 
-    public Book(String title, String author, String category) {
+    public Book(String title, String author, String category, String ISBN, byte quantity) {
         this.title = title;
         this.author = author;
         this.category = category;
+        this.ISBN = ISBN;
+        this.quantity = quantity;
         setDateAdded();
+        setBookID();
     }
 
     public void getBookDetails() {
@@ -21,10 +27,42 @@ public class Book {
     @Override
     public String toString() {
         return "Title: " + title + "\nAuthor: " + author + "\nBook ID: " + bookID +
-                "\nCategory: " + category + "\nDate Added: " + dateAdded;
+                "\nCategory: " + category + "\nQuantity: " + quantity + "\nDate Added: " + dateAdded;
     }
 
     private void setDateAdded(){
         this.dateAdded = LocalDate.now();
+    }
+
+    public void setQuantity(byte quantity) {
+        this.quantity = quantity;
+    }
+
+    private void setBookID() {
+        this.bookID = (byte) (Library.books.size() + 1);
+    }
+
+    public byte getQuantity(){
+        return quantity;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public byte getAmountBorrowed() {
+        return amountBorrowed;
     }
 }
