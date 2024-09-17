bd5468d11c95361ed676e1bf81e18bd54aed51cf
EmmaAdabra <adabraemma@gmail.com>
2024-09-17 17:17:42 +0100

    Modify readString and readByte method
    Both method signature were modified to accept string argument and a logic to handle user input were added

--- src/Console.java
+++ src/Console.java
@@ -1,4 +1,7 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * This class is used to read users inputs
@@ -6,15 +9,40 @@ import java.util.Scanner;
 public class Console {
     private static Scanner scanner = new Scanner(System.in);
 
-    public static String readString(){
-        return scanner.nextLine().trim().toUpperCase();
+    public static String readString(String prompt){
+        String value;
+        while(true) {
+            System.out.print(prompt +": ");
+            value = scanner.nextLine().toUpperCase().trim();
+            if(value.isEmpty()){
+                System.out.println(prompt + " can't be empty");
+            }
+            else {
+                break;
+            }
+        }
+        return value;
     }
 
-    public static byte readByte(){
-        return scanner.nextByte();
+    public static byte readByte(String prompt){
+        byte option;
+        while (true) {
+            try {
+                System.out.print(prompt +": ");
+                option = scanner.nextByte();
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("only accept numbers");
+                scannerNext();
+            }
+        }
+        return option;
     }
 
     public static void clearBuffer() {
         scanner.nextLine();
+    }
+    public static void scannerNext(){
+        scanner.next();
     }
 }
