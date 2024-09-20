f62c8f7cb4daed77809f112f08fea835a1f8340d
EmmaAdabra <adabraemma@gmail.com>
2024-09-20 23:18:11 +0100

    Edited string literal

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import UIs.RegisterUser;
 
 public class Main {
     public static void main(String []args){
+        System.out.println();
         System.out.println("                  Virtual Library                   ".toUpperCase());
         var registerUser = new RegisterUser();
         registerUser.registrationUI();
