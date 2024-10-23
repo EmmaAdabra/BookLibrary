d3146e80ce1cfce946928ff3b6245ee071fd0da2
EmmaAdabra <adabraemma@gmail.com>
2024-10-23 19:54:54 -0400

    Add AuthController class
    This class interact with user to get their login details and passed it to AuthServices for verification

---
+++ src/main/java/com/librarySystem/auth/AuthController.java
@@ -0,0 +1,48 @@
+package main.java.com.librarySystem.auth;
+
+import main.java.com.librarySystem.admin.AdminController;
+import main.java.com.librarySystem.repository.IUserRepository;
+import main.java.com.librarySystem.user.RUser;
+import main.java.com.librarySystem.user.UserController;
+import main.java.com.librarySystem.util.IValidateInput;
+import main.java.com.librarySystem.util.IterateInput;
+import main.java.com.librarySystem.util.NewResponse;
+
+public class AuthController {
+    UserController userController;
+    AdminController adminController;
+    IValidateInput validateInput;
+    IAuthServices authServices;
+
+    public AuthController(AdminController adminController,
+                          UserController userController, IUserRepository userRepository, IValidateInput validateInput) {
+        this.userController = userController;
+        this.adminController = adminController;
+        this.validateInput = validateInput;
+        this.authServices = new AuthServices(userRepository);
+    }
+
+    public void login(){
+        System.out.println();
+        System.out.println("--------------- Login Details ---------------");
+        String email = IterateInput.stringInput("email", validateInput::validateEmail);
+        String password = IterateInput.stringInput("password", validateInput::validatePassword);
+
+        NewResponse response = authServices.verifyLogin(email, password);
+        if(response.status) {
+           RUser loggedInUser = (RUser)response.obj;
+
+            switch (loggedInUser.getRole()){
+                case ADMIN -> {
+                    adminController.handleLogin(loggedInUser);
+                }
+                case USER -> {
+                    userController.handleLogin(loggedInUser);
+                }
+            }
+        } else {
+            System.out.println();
+            System.out.println(response.message);
+        }
+    }
+}
