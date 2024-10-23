b4c370ad0e218e99a5d217f2bc0fa031338486a7
EmmaAdabra <adabraemma@gmail.com>
2024-10-23 19:52:53 -0400

    Add IAuthServices interface
    This class implement IAuthServices, it defines the business logic need to authenticate and log users in.

---
+++ src/main/java/com/librarySystem/auth/AuthServices.java
@@ -0,0 +1,24 @@
+package main.java.com.librarySystem.auth;
+
+import main.java.com.librarySystem.repository.IUserRepository;
+import main.java.com.librarySystem.user.RUser;
+import main.java.com.librarySystem.util.IValidateInput;
+import main.java.com.librarySystem.util.NewResponse;
+
+public class AuthServices implements IAuthServices{
+    IUserRepository userRepository;
+
+    public AuthServices(IUserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public NewResponse verifyLogin(String email, String password) {
+        RUser user = userRepository.getUserByEmail(email);
+        if(user == null || !user.getUserPassword().equals(password)) {
+            return new NewResponse(false, "Invalid login details", null);
+        }
+
+        return new NewResponse(true, "success", user);
+    }
+}
