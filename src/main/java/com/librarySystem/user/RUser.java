e954e74b77aeedc77c4ac0c65a80695bd77b731a
EmmaAdabra <adabraemma@gmail.com>
2024-10-23 19:17:09 -0400

    Add RUser class
    This class represent user actors in the library system

---
+++ src/main/java/com/librarySystem/user/RUser.java
@@ -0,0 +1,69 @@
+package main.java.com.librarySystem.user;
+
+import main.java.com.librarySystem.model.Role;
+
+import java.util.Objects;
+
+public class RUser {
+    private String userName;
+    private String userEmail;
+    private String userPassword;
+    private Role role;
+
+    private boolean isLoggedIn;
+
+    public RUser(String name, String email, String password, Role role) {
+        this.userName = name;
+        this.userEmail = email;
+        this.userPassword = password;
+        this.role = role;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public boolean isLoggedIn() {
+        return isLoggedIn;
+    }
+
+    public void login(){
+        this.isLoggedIn = true;
+    }
+
+    public void logout(){
+        this.isLoggedIn = false;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "Name: " + userName + "\n" +
+                "Email: " + userEmail + "\n" +
+                "Role: " + role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof RUser user)) return false;
+        return getUserEmail().equals(user.getUserEmail());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getUserEmail());
+    }
+}
