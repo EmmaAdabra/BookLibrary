9d08624d2f9d05c62f34ac50d6c66f12a42c2ccc
EmmaAdabra <adabraemma@gmail.com>
2024-10-23 19:47:42 -0400

    Add AdminService class
    This class contains the business logic for admin users in system

---
+++ src/main/java/com/librarySystem/admin/AdminService.java
@@ -0,0 +1,45 @@
+package main.java.com.librarySystem.admin;
+
+import main.java.com.librarySystem.model.Book;
+import main.java.com.librarySystem.repository.IBookRepository;
+import main.java.com.librarySystem.repository.IBorrowedBookRepo;
+import main.java.com.librarySystem.repository.IUserRepository;
+import main.java.com.librarySystem.user.RUser;
+import main.java.com.librarySystem.user.UserService;
+import main.java.com.librarySystem.util.NewResponse;
+
+import java.util.Collections;
+import java.util.Map;
+
+
+public class AdminService extends UserService implements IAdminService {
+//    private IBookRepository bookRepository;
+//    private IUserRepository userRepository;
+
+    public AdminService(IUserRepository userRepository, IBookRepository bookRepository,
+                        IBorrowedBookRepo borrowedBooksRepo){
+        super(userRepository, bookRepository, borrowedBooksRepo);
+//        this.bookRepository = bookRepository;
+//        this.userRepository = userRepository;
+    }
+
+    @Override
+    public NewResponse addBook(Book book) {
+        if(bookRepository.getBookByISBN(book.getISBN()) != null){
+            return new NewResponse(false, book.getTitle() + " already exist", null);
+        }
+
+        bookRepository.addBook(book);
+        return new NewResponse(true, "success", null);
+    }
+
+    @Override
+    public Map<RUser, Map<Book, Integer>> getBorrowRecord() {
+        var borrowRecords = borrowedBooksRepo.getBorrowRecord();
+        if(!borrowRecords.isEmpty()){
+            return borrowRecords;
+        }
+
+        return Collections.emptyMap();
+    }
+}
