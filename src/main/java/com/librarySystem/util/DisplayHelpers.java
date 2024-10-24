00babd8190f8ea1626bbcb4cc7c14a33445c6902
EmmaAdabra <adabraemma@gmail.com>
2024-10-23 20:13:23 -0400

    Add DisplayHelpers class
    This is a utility class, that helps UserController in displaying content to the terminal

---
+++ src/main/java/com/librarySystem/util/DisplayHelpers.java
@@ -0,0 +1,33 @@
+package main.java.com.librarySystem.util;
+
+import main.java.com.librarySystem.model.Book;
+
+public class DisplayHelpers {
+    public static void displayMenu(String title, String[] options){
+        System.out.println();
+        System.out.println(title);
+        System.out.println();
+        for(int i = 0; i < options.length; i++) {
+            System.out.println(i+1 + ". " + options[i]);
+        }
+        System.out.println();
+    }
+
+    public static void displaySubMenu(String title, String[] options){
+//        System.out.println();
+        System.out.println(title);
+        for(int i = 0; i < options.length; i++) {
+            System.out.println(i+1 + ". " + options[i]);
+        }
+        System.out.println();
+    }
+
+    public static void displayBorrowedBook(Book book, int amountBorrowed){
+        String bookDetails = "Book Title: " + book.getTitle() + "\n"
+                + "Book Author: " + book.getAuthor() + "\n"
+                + "Category: " + book.getCategory() + "\n"
+                + "Quantity Borrowed: " + amountBorrowed;
+        System.out.println(bookDetails);
+    }
+
+}
