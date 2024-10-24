4169caf59c0e4ce4c1e6b2a7cb3074ce99bc1cc5
EmmaAdabra <adabraemma@gmail.com>
2024-10-23 20:16:48 -0400

    Modify Utils class
    Add a new method maskEmail that masked part of emails to be display to the terminal with asterisks

---
+++ src/main/java/com/librarySystem/util/Utils.java
@@ -0,0 +1,19 @@
+package main.java.com.librarySystem.util;
+
+public class Utils {
+    public static IValidateInput validate = new ValidateUserInput();
+
+    public static String maskEmail(String email) {
+        String [] splitEmail = email.split("@");
+        String emailName = splitEmail[0];
+        String emailDomain = splitEmail[1];
+        int emailNameLength = emailName.length();
+        int maskLength = emailNameLength / 2;
+
+        maskLength = emailNameLength % 2 > 0 ? maskLength + 1 : maskLength;
+        String maskCharacter = "*".repeat(maskLength);
+
+        return maskCharacter + emailName.substring(maskLength) + "@" + emailDomain;
+    }
+
+}
