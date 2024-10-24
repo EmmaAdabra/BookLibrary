b0bc7b08508e014b33f8d0a474c354eeb22a88c7
EmmaAdabra <adabraemma@gmail.com>
2024-10-23 20:18:20 -0400

    Deleted " src/Main.java " and " src/util/Utils.java" file

--- src/util/Utils.java
+++
@@ -1,8 +0,0 @@
-package util;
-
-import validateInput.ValidateInput;
-import validateInput.ValidateUserInput;
-
-public class Utils {
-    public static ValidateInput validate = new ValidateUserInput();
-}
