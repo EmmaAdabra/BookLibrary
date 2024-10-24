d0b462ec014447c50867ea02cbd9bcac6a902463
EmmaAdabra <adabraemma@gmail.com>
2024-10-23 21:10:09 -0400

    Modified to fix response first args data type

--- src/main/java/com/librarySystem/util/ValidateUserInput.java
+++ src/main/java/com/librarySystem/util/ValidateUserInput.java
@@ -12,9 +12,9 @@ public class ValidateUserInput implements IValidateInput {
     @Override
     public Response validateName(String name) {
         if(!(name.length() >= 3 && name.length() <= 20)){
-            return new Response(0, "username should be min 3 and max 20 character", null);
+            return new Response(false, "username should be min 3 and max 20 character", null);
         }
-        return new Response(1, "success", null);
+        return new Response(true, "success", null);
     }
 
     @Override
@@ -23,25 +23,25 @@ public class ValidateUserInput implements IValidateInput {
         var pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(email);
         if(!matcher.matches()) {
-            return new Response(0,
+            return new Response(false,
                      "Email should follow this format \"example@example.com\" ", null);
         }
-        return new Response(1, "success", null);
+        return new Response(true, "success", null);
     }
 
     @Override
     public Response validatePassword(String password) {
         if(!(password.length() >= 4)) {
-            return new Response(0, "password shouldn't be less than 4", null);
+            return new Response(false, "password shouldn't be less than 4", null);
         }
-        return new Response(1, "success", null);
+        return new Response(true, "success", null);
     }
 
     @Override
     public Response validateOption(int option, int min, int max) {
         if(!(option >= min && option <= max)){
-            return new Response(0, "option should be between " + min + " and " + max, null);
+            return new Response(false, "option should be between " + min + " and " + max, null);
         }
-        return new Response(1, "success", null);
+        return new Response(true, "success", null);
     }
 }
