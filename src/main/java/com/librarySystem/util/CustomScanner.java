47573ef1e40cbf0250c7dd74227583f51ea1c840
EmmaAdabra <adabraemma@gmail.com>
2024-10-25 02:31:43 -0400

    modified readInt and readString method

--- src/main/java/com/librarySystem/util/CustomScanner.java
+++ src/main/java/com/librarySystem/util/CustomScanner.java
@@ -7,12 +7,13 @@ import java.util.Scanner;
  */
 public class CustomScanner {
     private static Scanner scanner = new Scanner(System.in);
+    private static ValidateUserInput validate = new ValidateUserInput();
     public static String readString(String prompt){
         String value;
         while(true) {
             System.out.print(prompt +": ");
             value = scanner.nextLine().toUpperCase().trim();
-            if(value.isEmpty()){
+            if(validate.validateEmptyInput(value)){
                 System.out.println(prompt + " can't be empty");
             }
             else {
@@ -24,19 +25,27 @@ public class CustomScanner {
 
     public static int readInt(String prompt){
         int option;
+
         while (true) {
-            try {
-                System.out.print(prompt +": ");
-                String userOption = scanner.nextLine().trim();
-                if(userOption.isEmpty()){
-                    System.out.println(prompt + " can't be empty");
-                    continue;
-                }
-                option = Integer.parseInt(userOption);
-                break;
-            } catch (NumberFormatException e) {
-                System.out.println("only accept numbers");
+            System.out.print(prompt +": ");
+            String userOption = scanner.nextLine().trim();
+
+            if(validate.validateEmptyInput(userOption)){
+                System.out.println(prompt + " can't be empty");
+                continue;
             }
+
+            var response = validate.validateIntInput(userOption);
+
+            if(!response.status) {
+                System.out.println("Only accept numbers");
+                continue;
+            }
+
+            option = (Integer) response.obj;
+
+            break;
+
         }
         return option;
     }
